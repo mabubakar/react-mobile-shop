2a8293677f4fc2940df418ce4d6df9ad7243d5b6
Abubakar <chabubakar001@gmail.com>
2020-10-19 20:54:42 +0500

    using map method

--- src/App.js
+++ src/App.js
@@ -1,36 +1,27 @@
 import React from 'react'
 import Mobile from './CoddingAddict/Mobile'
 import './CoddingAddict/Mobile.css'
+import mobiles from './CoddingAddict/mobiles'
 
 function App() {
   return (
     <>
       <h1 className='text-center my-4 '>Mobile Shop</h1>
       <section className='mobile__list'>
-        <Mobile
-          company='Samsung'
-          model='Galaxy S20 FE'
-          imgUrl='https://www.whatmobile.com.pk/admin/images/Samsung/SamsungGalaxyS20FE-s.jpg'
-          price='Rs. 119,999'
-        />
-        <Mobile
-          company='Vivo'
-          model='V20'
-          imgUrl='https://www.whatmobile.com.pk/admin/images/Vivo/VivoV20-s.jpg'
-          price='Rs. 59,999'
-        />
-        <Mobile
-          company='Oppo'
-          model='F17 Pro'
-          imgUrl='https://www.whatmobile.com.pk/admin/images/Oppo/OppoF17Pro-s.jpg'
-          price='Rs. 51,999'
-        />
-        <Mobile
-          company='Infinix  '
-          model='Zero 8'
-          imgUrl='https://www.whatmobile.com.pk/admin/images/Infinix/InfinixZero8-s.jpg'
-          price='Rs. 39,999'
-        />
+        {mobiles.map(mobile => {
+          return (
+            <Mobile
+              key={mobile.id}
+              //using spread operator
+              {...mobile}
+              //or using this way
+              // company={mobile.company}
+              // model={mobile.model}
+              // price={mobile.price}
+              // imgUrl={mobile.imgUrl}
+            />
+          )
+        })}
       </section>
     </>
   )
---
+++ src/CoddingAddict/mobiles.js
@@ -0,0 +1,127 @@
+const mobiles = [
+  {
+    id: 1,
+    company: 'Samsung',
+    model: 'Galaxy S20 FE',
+    price: 'Rs. 119,999',
+    imgUrl:
+      'https://www.whatmobile.com.pk/admin/images/Samsung/SamsungGalaxyS20FE-s.jpg',
+  },
+  {
+    id: 2,
+    company: 'Vivo',
+    model: 'V20',
+    price: 'Rs. 59,999',
+    imgUrl: 'https://www.whatmobile.com.pk/admin/images/Vivo/VivoV20-s.jpg',
+  },
+  {
+    id: 3,
+    company: 'Oppo',
+    model: 'F17 Pro',
+    price: 'Rs. 51,999',
+    imgUrl: 'https://www.whatmobile.com.pk/admin/images/Oppo/OppoF17Pro-s.jpg',
+  },
+  {
+    id: 4,
+    company: 'Infinix',
+    model: 'Zero 8',
+    price: 'Rs. 39,999',
+    imgUrl:
+      'https://www.whatmobile.com.pk/admin/images/Infinix/InfinixZero8-s.jpg',
+  },
+  {
+    id: 5,
+    company: 'Realme',
+    model: 'C17',
+    price: 'Rs. 31,999',
+    imgUrl: 'https://www.whatmobile.com.pk/admin/images/Realme/RealmeC17-s.jpg',
+  },
+  {
+    id: 6,
+    company: 'Samsung',
+    model: 'Galaxy Note 20 Ultra',
+    price: 'Rs. 219,999',
+    imgUrl:
+      'https://www.whatmobile.com.pk/admin/images/Samsung/SamsungGalaxyNote20Ultra-s.jpg',
+  },
+  {
+    id: 7,
+    company: 'Oppo',
+    model: 'Reno 4',
+    price: 'Rs. 59,999',
+    imgUrl: 'https://www.whatmobile.com.pk/admin/images/Oppo/OppoReno4-s.jpg',
+  },
+  {
+    id: 8,
+    company: 'Realme',
+    model: 'X3 Super Zoom',
+    price: 'Rs. 79,999',
+    imgUrl:
+      'https://www.whatmobile.com.pk/admin/images/Realme/RealmeX3SuperZoom-s.jpg',
+  },
+  {
+    id: 9,
+    company: 'Xiaomi',
+    model: 'Mi Note 10 Lite',
+    price: 'Rs. 62,999',
+    imgUrl:
+      'https://www.whatmobile.com.pk/admin/images/Xiaomi/XiaomiMiNote10Lite-s.jpg',
+  },
+  {
+    id: 10,
+    company: 'Samsung',
+    model: 'Galaxy Note 20',
+    price: 'Rs. 179,999',
+    imgUrl:
+      'https://www.whatmobile.com.pk/admin/images/Samsung/SamsungGalaxyNote20-s.jpg',
+  },
+  {
+    id: 11,
+    company: 'Oppo',
+    model: 'Find X2 pro',
+    price: 'Rs. 179,999',
+    imgUrl:
+      'https://www.whatmobile.com.pk/admin/images/Oppo/OppoFindX2pro-s.jpg',
+  },
+  {
+    id: 12,
+    company: 'Samsung',
+    model: 'Galaxy S20 Plus',
+    price: 'Rs. 187,999',
+    imgUrl:
+      'https://www.whatmobile.com.pk/admin/images/Samsung/SamsungGalaxyS20Plus-s.jpg',
+  },
+  {
+    id: 13,
+    company: 'Xiaomi',
+    model: 'Mi 10',
+    price: 'Rs. 149,999',
+    imgUrl:
+      'https://www.whatmobile.com.pk/admin/images/Xiaomi/XiaomiMi10-s.jpg',
+  },
+  {
+    id: 14,
+    company: 'Xiaomi',
+    model: 'Mi Note 10',
+    price: 'Rs. 79,999',
+    imgUrl:
+      'https://www.whatmobile.com.pk/admin/images/Xiaomi/XiaomiMiNote10-s.jpg',
+  },
+  {
+    id: 15,
+    company: 'Samsung',
+    model: 'Galaxy A71',
+    price: 'Rs. 67,999',
+    imgUrl:
+      'https://www.whatmobile.com.pk/admin/images/Samsung/SamsungGalaxyA71-s.jpg',
+  },
+  {
+    id: 16,
+    company: 'Apple',
+    model: 'iPhone 11 Pro Max',
+    price: 'Rs. 234,500',
+    imgUrl:
+      'https://www.whatmobile.com.pk/admin/images/Apple/AppleiPhone11ProMax-s.jpg',
+  },
+]
+export default mobiles
